2c185e62bd1c8d12426b4b8176638d6999fcc9d7
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-06 17:27:34 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+"""CF Sample App Python"""
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+port = int(os.getenv("PORT", "3000"))
+
+@app.route('/')
+def hello_world():
+  return 'Hello World! I am running on port ' + str(port)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=port)
