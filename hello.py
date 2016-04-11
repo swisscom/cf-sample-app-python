ccdb9ffd82193cf228beda72988de447990a3368
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-11 18:32:25 +0200

    remove double quotes

--- hello.py
+++ hello.py
@@ -1,15 +1,13 @@
-"""CF Sample App Python"""
-
 from flask import Flask
 import os
 
 app = Flask(__name__)
 
-port = int(os.getenv("PORT", "3000"))
+port = int(os.getenv('PORT', '3000'))
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
+    return 'Congratulations! Welcome to the Swisscom Application Cloud.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
