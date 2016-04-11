392abee59e70bcfab01e153f45c78bee91268a2f
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-11 23:45:11 +0200

    add comments

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
+# import dependencies
 import os
 from flask import Flask
 
+# bootstrap the app
 app = Flask(__name__)
 
+# set the port dynamically with a default of 3000 for local development
 port = int(os.getenv('PORT', '3000'))
 
+# our base route which just returns a string
 @app.route('/')
 def hello_world():
     return 'Congratulations! Welcome to the Swisscom Application Cloud.'
 
+# start the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
