22bd41d5f7289b5ee608ac330f166b9b8b2560cb
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-11 08:10:21 +0200

    indent by 4 spaces

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ port = int(os.getenv("PORT", "3000"))
 
 @app.route('/')
 def hello_world():
-  return 'Hello World! I am running on port ' + str(port)
+    return 'Hello World! I am running on port ' + str(port)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
